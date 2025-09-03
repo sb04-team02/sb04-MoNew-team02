e766ba990ba4fd3601883d50e4127b1263d8677f
ShinEunSu <94344629+Shinsu98@users.noreply.github.com>
2025-09-03 17:26:17 +0900

    feat: 누락된 Like 도메인의 Service 추가

---
+++ src/main/java/com/sprint/team2/monew/domain/like/service/basic/BasicReactionService.java
@@ -0,0 +1,8 @@
+package com.sprint.team2.monew.domain.like.service.basic;
+
+import com.sprint.team2.monew.domain.like.service.ReactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BasicReactionService implements ReactionService {
+}
