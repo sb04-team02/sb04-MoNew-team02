e69a2be959c57a8b90cb4f7bfc2fdce5119d3970
NaNHangBok <kms_1015@naver.com>
2025-09-03 15:51:24 +0900

    feat: 로그 관리 설정 추가
    - 로그를 관리하기 위해 logback-spring.xml 파일 추가
    - MDCLoggingInterceptor.java Config 추가
    - MDCLogginginterceptor가 추가됨에 따라 WebConfig 내용 수정

---
+++ src/main/java/com/sprint/team2/monew/global/config/MDCLoggingInterceptor.java
@@ -0,0 +1,25 @@
+package com.sprint.team2.monew.global.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+
+@Component
+public class MDCLoggingInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        MDC.put("requestId", UUID.randomUUID().toString());
+        MDC.put("requestMethod", request.getMethod());
+        MDC.put("requestUrl", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        MDC.clear();
+    }
+}
