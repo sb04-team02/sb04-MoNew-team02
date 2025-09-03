384cf19d61d80d579a06b55d1d2869d6d5330a5d
JihyeonLee <devlee1011@gmail.com>
2025-09-03 18:20:19 +0900

    refactor(예외 처리): BaseErrorCode status 필드 로직 변경
    단순 넘버링(400, 500) -> HttpStatus.value()로 변경

--- src/main/java/com/sprint/team2/monew/global/error/BaseErrorCode.java
+++ src/main/java/com/sprint/team2/monew/global/error/BaseErrorCode.java
@@ -2,11 +2,12 @@ package com.sprint.team2.monew.global.error;
 
 import com.sprint.team2.monew.global.constant.ErrorCode;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum BaseErrorCode implements ErrorCode {
-    INVALID_INPUT_VALUE(400, "잘못된 입력값입니다."),
-    INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다.");
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST.value(), "잘못된 입력값입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류가 발생했습니다.");
 
     private final int status;
     private final String message;
