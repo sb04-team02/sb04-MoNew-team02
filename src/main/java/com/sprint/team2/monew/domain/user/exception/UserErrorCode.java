3c696ea3f5823f7a3dcdf7cf550124db3de7abea
JihyeonLee <devlee1011@gmail.com>
2025-09-03 18:13:51 +0900

    refactor(예외 처리): 사용자 - 에러 코드 status 로직 변경
    단순 번호에서 HttpStatus.value()로 명확하게 변경

--- src/main/java/com/sprint/team2/monew/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/sprint/team2/monew/domain/user/exception/UserErrorCode.java
@@ -3,15 +3,16 @@ package com.sprint.team2.monew.domain.user.exception;
 import com.sprint.team2.monew.global.constant.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-    USER_NOT_FOUND(404, "사용자 정보가 없습니다."),
-    EMAIL_ALREADY_EXISTS(409, "중복된 이메일로 가입할 수 없습니다."),
-    INVALID_USER_CREDENTIALS(401, "이메일 또는 비밀번호가 일치하지 않습니다."),
-    FORBIDDEN_USER_UPDATE(403, "해당 사용자에 대한 수정 권한이 없습니다."),
-    FORBIDDEN_USER_DELETE(403, "해당 사용자에 대한 삭제 권한이 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "사용자 정보가 없습니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "중복된 이메일로 가입할 수 없습니다."),
+    INVALID_USER_CREDENTIALS(HttpStatus.UNAUTHORIZED.value(), "이메일 또는 비밀번호가 일치하지 않습니다."),
+    FORBIDDEN_USER_UPDATE(HttpStatus.FORBIDDEN.value(), "해당 사용자에 대한 수정 권한이 없습니다."),
+    FORBIDDEN_USER_DELETE(HttpStatus.FORBIDDEN.value(), "해당 사용자에 대한 삭제 권한이 없습니다.");
 
     private final int status;
     private final String message;
