ff4d681558d62d874d63d94b8c5de2e587174ad5
joongwon <anjoongwon517@gmail.com>
2025-09-03 13:28:40 +0900

    fix: ArticleRepository에서 pk 형식 변경

--- src/main/java/com/sprint/team2/monew/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/sprint/team2/monew/domain/article/repository/ArticleRepository.java
@@ -3,5 +3,7 @@ package com.sprint.team2.monew.domain.article.repository;
 import com.sprint.team2.monew.domain.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+import java.util.UUID;
+
+public interface ArticleRepository extends JpaRepository<Article, UUID> {
 }
