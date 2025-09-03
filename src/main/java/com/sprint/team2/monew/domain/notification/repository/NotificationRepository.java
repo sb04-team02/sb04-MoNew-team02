43cebf486a21019c81e0ced3df3756f14d73b839
문은서 <meunse1999@naver.com>
2025-09-03 18:51:50 +0900

    feat: 알림 관리 - 도메인 계층 분리 #82
    - Service, Repository, Mapper 인터페이스 정의

---
+++ src/main/java/com/sprint/team2/monew/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,21 @@
+package com.sprint.team2.monew.domain.notification.repository;
+
+import com.sprint.team2.monew.domain.notification.entity.Notification;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    Slice<Notification> findAllByUserIdOrderByCreatedAtDesc(
+            UUID userId,
+            LocalDateTime nextAfter,
+            Pageable pageable);
+
+    List<Notification> findAllByUserIdAndConfirmedIsFalse(UUID userId);
+
+    void deleteByConfirmedIsTrue();
+}
