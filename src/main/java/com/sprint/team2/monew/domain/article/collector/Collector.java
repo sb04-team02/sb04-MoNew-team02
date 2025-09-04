ecc031db64ac7e67f57b48fb81a2f4a852007dbd
joongwon <anjoongwon517@gmail.com>
2025-09-04 11:03:09 +0900

    Revert "feat: 뉴스 기사 수집용 인터페이스 정의"
    This reverts commit 74ae6b44c77c37594ab04e4ad13956f419d1df6a.

--- src/main/java/com/sprint/team2/monew/domain/article/collector/Collector.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.team2.monew.domain.article.collector;
-
-import com.sprint.team2.monew.domain.article.dto.response.CursorPageResponseArticleDto;
-
-public interface Collector {
-    CursorPageResponseArticleDto collect(String keyword, int size, String cursor);
-}
