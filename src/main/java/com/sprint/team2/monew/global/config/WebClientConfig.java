bf7219144d218615152964ed252b72a496e345f7
joongwon <anjoongwon517@gmail.com>
2025-09-05 12:44:57 +0900

    feat: base url 추가

--- src/main/java/com/sprint/team2/monew/global/config/WebClientConfig.java
+++ src/main/java/com/sprint/team2/monew/global/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.sprint.team2.monew.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,7 +8,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient WebClient() {
-        return WebClient.builder().build();
+    public WebClient WebClient(@Value("${naver.api.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
     }
 }
