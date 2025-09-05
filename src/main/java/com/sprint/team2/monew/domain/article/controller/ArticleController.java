66bafd15a5d000cccebf3e9ed5fce3367222b36b
joongwon <anjoongwon517@gmail.com>
2025-09-05 21:23:06 +0900

    feat: DB 저장 확인을 위한 임시 controller 코드

--- src/main/java/com/sprint/team2/monew/domain/article/controller/ArticleController.java
+++ src/main/java/com/sprint/team2/monew/domain/article/controller/ArticleController.java
@@ -1,7 +1,25 @@
 package com.sprint.team2.monew.domain.article.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.sprint.team2.monew.domain.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/articles")
 public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveArticle(@RequestHeader UUID interestId) {
+        articleService.saveByInterest(interestId);
+
+        return ResponseEntity.ok().build();
+    }
 }
