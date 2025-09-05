4c00c64122a780844bb65141a22a927b4671afec
joongwon <anjoongwon517@gmail.com>
2025-09-05 12:40:49 +0900

    feat: api호출 실패, 빈 응답 반화하는 에러코드 정의

--- src/main/java/com/sprint/team2/monew/domain/article/exception/ArticleErrorCode.java
+++ src/main/java/com/sprint/team2/monew/domain/article/exception/ArticleErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ArticleErrorCode implements ErrorCode {
-    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "뉴스 기사 정보가 없습니다.");
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "뉴스 기사 정보가 없습니다."),
+    NAVER_API_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Naver API 호출 실패"),
+    EMPTY_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Naver API가 빈 응답 반환"),
+    ;
 
     private final int status;
     private final String message;
