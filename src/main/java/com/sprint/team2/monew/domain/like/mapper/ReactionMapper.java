76b47d80be0abb78d5c958166420212a3220939e
ShinEunSu <94344629+Shinsu98@users.noreply.github.com>
2025-09-05 20:23:19 +0900

    feat(댓글): 관심사 댓글 좋아요 생성 기능 추가

--- src/main/java/com/sprint/team2/monew/domain/like/mapper/ReactionMapper.java
+++ src/main/java/com/sprint/team2/monew/domain/like/mapper/ReactionMapper.java
@@ -1,7 +1,21 @@
 package com.sprint.team2.monew.domain.like.mapper;
 
+import com.sprint.team2.monew.domain.like.dto.CommentLikeDto;
+import com.sprint.team2.monew.domain.like.dto.response.CommentLikeActivityDto;
+import com.sprint.team2.monew.domain.like.entity.Reaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReactionMapper {
+    @Mapping(target = "likedBy", source = "reaction.user.id")
+    @Mapping(target = "commentId", source = "reaction.comment.id")
+    @Mapping(target = "articleId", source = "reaction.comment.article.id")
+    @Mapping(target = "commentUserId", source = "reaction.comment.user.id")
+    @Mapping(target = "commentUserNickname", source = "reaction.comment.user.nickname")
+    @Mapping(target = "commentContent", source = "reaction.comment.content")
+    @Mapping(target = "commentLikeCount", source = "reaction.comment.likeCount")
+    @Mapping(target = "commentCreatedAt", source = "reaction.comment.createdAt")
+    CommentLikeDto toDto(Reaction reaction);
+
 }
--- src/main/java/com/sprint/team2/monew/domain/like/repository/ReactionRepository.java
+++ src/main/java/com/sprint/team2/monew/domain/like/repository/ReactionRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ReactionRepository extends JpaRepository<Reaction, UUID> {
+    //중복 체크를 위한 구문
+    boolean existsByUserIdAndCommentId(UUID userId, UUID commentId);
 }
