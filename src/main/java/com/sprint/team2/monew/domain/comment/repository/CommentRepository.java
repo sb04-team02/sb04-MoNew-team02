6075d6e0dddcf2e389158b9bcd21685e3259d616
ShinEunSu <94344629+Shinsu98@users.noreply.github.com>
2025-09-08 14:15:24 +0900

    fix : BasicReactionService likeComment 메소드에서 CommentLikeDto도 좋아요증가시 반영되도록 수정

--- src/main/java/com/sprint/team2/monew/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sprint/team2/monew/domain/comment/repository/CommentRepository.java
@@ -11,6 +11,11 @@ import java.util.UUID;
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("update Comment c set c.likeCount = c.likeCount + 1 where c.id = :id")
-    int incrementLikeCount(@Param("id") UUID commentId);
+    @Query( value = """
+        update comments
+            set like_count = like_count + 1
+        where id = :id
+        returning like_count
+    """, nativeQuery = true)
+    long incrementLikeCountReturning(@Param("id") UUID commentId);
 }
--- src/main/java/com/sprint/team2/monew/domain/like/mapper/ReactionMapper.java
+++ src/main/java/com/sprint/team2/monew/domain/like/mapper/ReactionMapper.java
@@ -18,4 +18,13 @@ public interface ReactionMapper {
     @Mapping(target = "commentCreatedAt", source = "reaction.comment.createdAt")
     CommentLikeDto toDto(Reaction reaction);
 
+    @Mapping(target = "likedBy",            source = "reaction.user.id")
+    @Mapping(target = "commentId",          source = "reaction.comment.id")
+    @Mapping(target = "articleId",          source = "reaction.comment.article.id")
+    @Mapping(target = "commentUserId",      source = "reaction.comment.user.id")
+    @Mapping(target = "commentUserNickname",source = "reaction.comment.user.nickname")
+    @Mapping(target = "commentContent",     source = "reaction.comment.content")
+    @Mapping(target = "commentLikeCount",   source = "updatedLikeCount")
+    @Mapping(target = "commentCreatedAt",   source = "reaction.comment.createdAt")
+    CommentLikeDto toDto(Reaction reaction, long updatedLikeCount);
 }
