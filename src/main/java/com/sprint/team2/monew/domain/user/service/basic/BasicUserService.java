0f0647376a87d04f4988dc3b7f5852f0d391667b
JihyeonLee <devlee1011@gmail.com>
2025-09-08 11:28:32 +0900

    refactor: 서비스 메서드에 디버깅용 로그 추가 (update, deleteLogically)
    update - 수정 전 nickname 로깅
    deleteLogically - id와 추가된 deletedAt 로깅

--- src/main/java/com/sprint/team2/monew/domain/user/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/team2/monew/domain/user/service/basic/BasicUserService.java
@@ -92,6 +92,7 @@ public class BasicUserService implements UserService {
                     return UserNotFoundException.withId(userId);
                 });
 
+        log.debug("[사용자] 정보 수정 전 - nickname={}", user.getNickname());
         user.update(request.nickname());
         UserDto userDto = userMapper.toDto(user);
         log.info("[사용자] 정보 수정 성공 - id={}, nickname={}", userDto.id(), userDto.nickname());
@@ -116,6 +117,7 @@ public class BasicUserService implements UserService {
                 });
 
         user.setDeletedAt(LocalDateTime.now());
+        log.debug("[사용자] 논리적 삭제 수행 - id={}, deletedAt={}", userId, user.getDeletedAt());
         log.info("[사용자] 논리적 삭제 성공 - id={}", userId);
     }
 
