43225e494b30911b660f795f39b8197f8ac28b3a
joongwon <anjoongwon517@gmail.com>
2025-09-09 14:07:00 +0900

    feat: 커서 페이지네이션 및 필터링 기능 추가

--- src/main/java/com/sprint/team2/monew/domain/article/controller/ArticleController.java
+++ src/main/java/com/sprint/team2/monew/domain/article/controller/ArticleController.java
@@ -1,6 +1,6 @@
 package com.sprint.team2.monew.domain.article.controller;
 
-import com.sprint.team2.monew.domain.article.dto.response.ArticleDto;
+import com.sprint.team2.monew.domain.article.dto.response.CursorPageResponseArticleDto;
 import com.sprint.team2.monew.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,18 +21,24 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping
-    public ResponseEntity<List<ArticleDto>> getArticles(@RequestHeader("Monew-Request-User-ID") UUID userId,
-                                                        @RequestParam(required = false) String keyword,
-                                                        @RequestParam(required = false) UUID interestId,
-                                                        @RequestParam(required = false) List<String> sourceIn,
-                                                        @RequestParam(required = false) LocalDateTime publishedDateFrom,
-                                                        @RequestParam(required = false) LocalDateTime publishedDateTo,
-                                                        @RequestParam(defaultValue = "publishDate") String orderBy,
-                                                        @RequestParam(defaultValue = "ASC") String direction,
-                                                        @RequestParam(required = false) String cursor,
-                                                        @RequestParam(required = false) LocalDateTime after,
-                                                        @RequestParam(defaultValue = "50") int limit) {
-        List<ArticleDto> result = articleService.read(userId, orderBy, direction, limit);
+    // TODO: ENUM 사용
+    public ResponseEntity<CursorPageResponseArticleDto> getArticles(@RequestHeader("Monew-Request-User-ID") UUID userId,
+                                                                    @RequestParam(required = false) String keyword,
+                                                                    @RequestParam(required = false) UUID interestId,
+                                                                    @RequestParam(required = false) List<String> sourceIn,
+                                                                    @RequestParam(required = false) LocalDateTime publishedDateFrom,
+                                                                    @RequestParam(required = false) LocalDateTime publishedDateTo,
+                                                                    @RequestParam(defaultValue = "publishDate") String orderBy,
+                                                                    @RequestParam(defaultValue = "ASC") String direction,
+                                                                    @RequestParam(required = false) String cursor,
+                                                                    @RequestParam(required = false) LocalDateTime after,
+                                                                    @RequestParam(defaultValue = "50") int limit) {
+        CursorPageResponseArticleDto result = articleService.read(
+                userId, orderBy, direction, limit,
+                keyword,
+                interestId, sourceIn, publishedDateFrom, publishedDateTo,
+                cursor, after
+        );
 
         return ResponseEntity
                 .status(HttpStatus.OK)
