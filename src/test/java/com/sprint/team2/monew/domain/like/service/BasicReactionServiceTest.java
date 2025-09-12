2e3fa2899273781f0d3ba68b2c02e8f3c338393c
Leejun <estherleejunkim@gmail.com>
2025-09-12 16:16:51 +0900

    fix: BasicReactionServiceTest에 publisher mock 추가

--- src/test/java/com/sprint/team2/monew/domain/like/service/BasicReactionServiceTest.java
+++ src/test/java/com/sprint/team2/monew/domain/like/service/BasicReactionServiceTest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
@@ -46,6 +47,9 @@ public class BasicReactionServiceTest {
 
     @InjectMocks
     BasicReactionService basicReactionService;
+
+    @Mock
+    private ApplicationEventPublisher publisher;
 
     private UUID commentId;
     private UUID requesterUserId;
