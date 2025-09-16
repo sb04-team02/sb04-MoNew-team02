ceb7adae53d0b2020f72cc05635b39d05a572775
Leejun <estherleejunkim@gmail.com>
2025-09-16 14:07:26 +0900

    feat: BasicArticleStorageService의 restoreArticle 메서드 작성

--- src/main/java/com/sprint/team2/monew/domain/article/service/ArticleStorageService.java
+++ src/main/java/com/sprint/team2/monew/domain/article/service/ArticleStorageService.java
@@ -1,9 +1,9 @@
 package com.sprint.team2.monew.domain.article.service;
 
 import com.sprint.team2.monew.domain.article.dto.response.ArticleRestoreResultDto;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public interface ArticleStorageService {
   void backupToS3(String filename, String article);
-  ArticleRestoreResultDto restoreArticle(LocalDateTime from, LocalDateTime to);
+  ArticleRestoreResultDto restoreArticle(LocalDate from, LocalDate to);
 }
--- src/main/java/com/sprint/team2/monew/domain/article/service/basic/BasicArticleStorageService.java
+++ src/main/java/com/sprint/team2/monew/domain/article/service/basic/BasicArticleStorageService.java
@@ -1,33 +1,35 @@
 package com.sprint.team2.monew.domain.article.service.basic;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sprint.team2.monew.domain.article.dto.response.ArticleRestoreResultDto;
 import com.sprint.team2.monew.domain.article.entity.Article;
 import com.sprint.team2.monew.domain.article.repository.ArticleRepository;
 import com.sprint.team2.monew.domain.article.service.ArticleStorageService;
 import com.sprint.team2.monew.domain.base.BaseEntity;
 import com.sprint.team2.monew.global.config.aws.S3Properties;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
-import javax.inject.Qualifier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 @Slf4j
 @Service
@@ -36,9 +38,8 @@ public class BasicArticleStorageService implements ArticleStorageService {
 
   private final S3Client s3Client;
   private final S3Properties s3Properties;
-  private final JobLauncher jobLauncher;
-  private final Job restoreNewsJob;
   private final ArticleRepository articleRepository;
+  private final ObjectMapper objectMapper;
 
   @Override
   public void backupToS3(String filename, String aggregateJson){
@@ -61,7 +62,6 @@ public class BasicArticleStorageService implements ArticleStorageService {
       );
       log.info("[뉴스 기사] 백업 완료 - filename ={}", filename);
 
-
     } catch (S3Exception e) {
       log.error("[뉴스 기사] 백업 오류 (S3) : {}", e.awsErrorDetails().errorMessage());
 
@@ -71,52 +71,83 @@ public class BasicArticleStorageService implements ArticleStorageService {
   }
 
   @Override
-  public ArticleRestoreResultDto restoreArticle(LocalDateTime from, LocalDateTime to) {
-    try {
-      log.info("[뉴스 기사] 뉴스 기사 복구 배치 실행 시작");
-      String backupDate = LocalDate.now().minusDays(1)
-          .format(DateTimeFormatter.ISO_LOCAL_DATE);
+  public ArticleRestoreResultDto restoreArticle(LocalDate from, LocalDate to) {
+    String bucket = s3Properties.bucket();
+    List<Article> missingArticlesTotal = new ArrayList<>();
 
-      JobParameters params = new JobParametersBuilder()
-          .addString("backupDate", backupDate)
-          .addLong("timestamp", System.currentTimeMillis())
-          .toJobParameters();
+    // 날짜 사이에 있는 모든 Article url fetch
+    Set<String> existingArticleUrls = articleRepository.findArticleUrlsBetweenDates(
+        from.atStartOfDay(), // 하루 시작
+        to.plusDays(1).atStartOfDay() // 다음날 하루 시작
+    );
+    log.info("[뉴스 기사 복구] S3로부터 뉴스 기사 복구 시작. 기간: {} ~ {}", from, to);
 
-      JobExecution jobExecution = jobLauncher.run(restoreNewsJob, params);
+    // from, to 사이에 있는 날짜 루프
+    for (LocalDate date = from; !date.isAfter(to); date = date.plusDays(1)) {
+      List<Article> articlesFromS3 = new ArrayList<>();
+      String prefix = String.format(
+          "articles-%s/",
+          date.format(DateTimeFormatter.ISO_LOCAL_DATE)
+      );
 
-      if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-        Collection<StepExecution> stepExecutions = jobExecution.getStepExecutions();
-        StepExecution restoreStepExecution = stepExecutions.iterator().next();
+      log.info("[뉴스 기사 복구] S3 디렉토리 처리 중: {}", prefix);
+      ListObjectsV2Request listReq = ListObjectsV2Request.builder()
+          .bucket(bucket)
+          .prefix(prefix)
+          .build();
+      ListObjectsV2Response listRes = s3Client.listObjectsV2(listReq);
 
-        List<Article> articles = (List<Article>) restoreStepExecution.getExecutionContext().get("items");
+      // 현재 날짜 디렉토리에 있는 파일들 루프
+      for (S3Object summary : listRes.contents()) {
+        String chunkFileKey = summary.key();
+        log.info("[뉴스 기사 복구] 청크 파일 읽는 중: {}", chunkFileKey);
 
-        if (articles == null) {
-          articles = new ArrayList<>();
+        GetObjectRequest getReq = GetObjectRequest.builder()
+            .bucket(bucket)
+            .key(chunkFileKey)
+            .build();
+
+        try (ResponseInputStream<GetObjectResponse> s3ObjectStream = s3Client.getObject(getReq)) {
+          List<Article> articlesFromChunk = objectMapper.readValue(
+              s3ObjectStream,
+              new TypeReference<>() {
+              }
+          );
+          if (articlesFromChunk != null && !articlesFromChunk.isEmpty()) {
+            articlesFromS3.addAll(articlesFromChunk);
+          }
+        } catch (S3Exception | IOException e) {
+          log.error("[뉴스 기사 복구] S3 prefix에서 파일 읽는 중 오류 발생. Prefix: {}", prefix, e);
         }
-
-        // save to db
-        articleRepository.saveAll(articles);
-
-        List<UUID> articleIds = articles.stream()
-            .map(BaseEntity::getId)
-            .toList();
-
-      log.info("[뉴스 기사] 뉴스 기사 복구 배치 실행 완료");
-        return new ArticleRestoreResultDto(
-            LocalDateTime.now(),
-            articleIds,
-            articles.size()
-        );
-
-      } else {
-        log.error("[뉴스 기사] 복구 배치 실패. Status: {}", jobExecution.getStatus());
       }
 
-    } catch (Exception e) {
-      log.error("[뉴스 기사] 뉴스 기사 복구 배치 실행 실패", e);
-    }
-    return new ArticleRestoreResultDto(LocalDateTime.now(), new ArrayList<>(), 0);
-  }
+      // DB에 있는거 필터링
+      List<Article> missingArticles = articlesFromS3.stream()
+          .filter(article -> !existingArticleUrls.contains(article.getSourceUrl()))
+          .toList();
 
+      if (!missingArticles.isEmpty()) {
+        log.info("[뉴스 기사 복구] 날짜 {}에 {}개의 유실된 기사를 찾았습니다.", date, missingArticles.size());
+        missingArticlesTotal.addAll(missingArticles);
+      }
+    } // 루프 끝
+
+    if (missingArticlesTotal.isEmpty()) {
+      log.info("[뉴스 기사 복구] 유실된 기사가 없어 복구를 종료합니다.");
+      return new ArticleRestoreResultDto(LocalDateTime.now(), new ArrayList<>(), 0);
+    }
+
+    articleRepository.saveAll(missingArticlesTotal);
+    log.info("[뉴스 기사 복구] 총 {}개의 뉴스 기사를 DB에 저장했습니다.", missingArticlesTotal.size());
+    List<UUID> savedArticleIdsTotal = missingArticlesTotal.stream()
+        .map(BaseEntity::getId)
+        .toList();
+    log.info("[뉴스 기사 복구] 총 {}개의 뉴스 기사 복구 완료.기간: {} ~ {}", savedArticleIdsTotal.size(), from, to);
+    return new ArticleRestoreResultDto(
+        LocalDateTime.now(),
+        savedArticleIdsTotal,
+        savedArticleIdsTotal.size()
+    );
+  }
 }
 
